107bb5f3d3e94f6439092ff634859c4a6a720629
ANGELR27 <angelrodriguez270520@gmail.com>
2025-09-07 11:10:47 -0500

    localstorage-categorias

--- MotoHub/src/js/dashboard_admin.js
+++ MotoHub/src/js/dashboard_admin.js
@@ -1,18 +1,26 @@
 const nombre_p = document.getElementById("nombre_p");
 const descripcion_p = document.getElementById("descripcion_p");
+const categoria_p = document.getElementById("categoria_p");
 
 const talla_p = document.getElementById("talla_p");
 const precio_p = document.getElementById("precio_p");
 
 const nombre = document.getElementById("nombre");
 const descripcion = document.getElementById("descripcion");
+const categoria = document.getElementById("categoria");
 const talla = document.getElementById("talla");
 const precio = document.getElementById("precio");
 
 const imagen = document.getElementById("imagen_p");
 
+const bt_guardar = document.getElementById("guardar");
+
 nombre.addEventListener("input", () => {
   nombre_p.textContent = nombre.value;
+});
+
+categoria.addEventListener("change", () => {
+  categoria_p.textContent = categoria.value;
 });
 
 descripcion.addEventListener("input", () => {
@@ -25,4 +33,19 @@ talla.addEventListener("input", () => {
 
 precio.addEventListener("input", () => {
   precio_p.textContent = precio.value;
+});
+
+bt_guardar.addEventListener("click", () => {
+  const producto = {
+    imagen: imagen.value,
+    nombre: nombre.value,
+    descripcion: descripcion.value,
+    talla: talla.value,
+    precio: precio.value,
+  };
+
+  const productos = JSON.parse(localStorage.getItem("productos")) || [];
+  productos.push(producto);
+  localStorage.setItem("productos", JSON.stringify(productos));
+  alert("Producto guardado con éxito!");
 });
